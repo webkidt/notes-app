3bba60d90c4c00ce0a8813be0438b4cc00097be4
David Pam <niratpam@gmail.com>
2022-11-11 05:33:09 +0100

    Lazy initialize notes state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
 import Editor from './components/Editor';
 import Sidebar from './components/Sidebar';
-import { data } from './data';
 import Split from 'react-split';
 import { nanoid } from 'nanoid';
 import './App.css';
 
 function App() {
-  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('notes')) || []);
+  const [notes, setNotes] = useState(() => JSON.parse(localStorage.getItem('notes')) || []);
   const [currentNoteId, setCurrentNoteId] = useState((notes[0] && notes[0].id) || '');
 
   useEffect(() => {
